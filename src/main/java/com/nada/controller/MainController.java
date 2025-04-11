4915e6695da0b29e11f8756f702241d39c7e45a8
user01 <user01@example.com>
2025-04-11 16:19:57 +0900

    4th commit

--- src/main/java/com/nada/controller/MainController.java
+++ src/main/java/com/nada/controller/MainController.java
@@ -11,7 +11,7 @@ public class MainController {
 	
 	@GetMapping
 	public String intro(Model model) {
-		model.addAttribute("greeting", "안녕하세요");
+		model.addAttribute("greeting", "jenkins ci/cd");
 		return "intro";
 	}
 	
